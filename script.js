b4221e7c1e3153ff9fed63b27dfe48942f631921
Mikhail Komyshev <rest.liandri@gmail.com>
2020-04-27 02:39:20 +0300

    add nesting

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ homeButtonFunc();
 
    
 function generateAvatarHtml(name, imageName) {
-  let ht = '<div class="personCard">\
+  let ht = '<div class="personCard" onclick="handler1(&#39;' + name + '&#39;)">\
 \
                 <div class="gradient-border sizeav person_card">\
                     <img src="./img/' + imageName + '" class="avatar sizeav">\
@@ -31,7 +31,7 @@ function generateAvatarHtml(name, imageName) {
 }
 
 function generatePersonCardHtml(personCard) {
-  let ht = '<div class="person-card">\
+  let ht = '<div class="person-card" onclick="handler1(&#39;' + personCard.name + '&#39;)">\
  <!-- style="width: 188px;height: 188px;" -->\
      <!-- style="width: 280px;height: 352px;" -->\
         <div class="gradient-border2 sizeofavatar">\
@@ -54,12 +54,12 @@ function generatePersonCardHtml(personCard) {
 
 function getNamefromHtml(strHtml) {
   let name = strHtml.substring(strHtml.lastIndexOf("/")+1);
-  return name.slice(0, -4);
+  return name;
 }
 
 function changePicName(strHtml, name) {
   //let str = strHtml.substring(0, strHtml.lastIndexOf("/")+1) + name + strHtml.substring(strHtml.lastIndexOf("."));
-  let str = strHtml.substring(0, strHtml.lastIndexOf("/")+1) + name + ".jpg";
+  let str = strHtml.substring(0, strHtml.lastIndexOf("/")+1) + name;
   return (str);
 }
 
@@ -71,14 +71,27 @@ frame3 = document.querySelector('.frame3');
 
 
 let array = global.inputArray
+let topElement = document.querySelector('.top_element');
 
 function makeHomeReal() {
+    
+    topElement.src = changePicName(topElement.src, 'logo.png');
+    
+    document.querySelector('.top_element_outer').classList.remove('gradient-border');
+    document.querySelector('.top_element_outer').classList.remove('top_element_round_size');
+    document.querySelector('.top_element').classList.remove('top_element_round_size');
+    document.querySelector('.top_element').classList.remove('avatar');
+    document.querySelector('.top_element').classList.add('top_element_default_size');
+    
+    
     frame3.innerHTML = '';
     for (let a of array) {
         if (!a.parent) {
             frame3.innerHTML += generateAvatarHtml(a.name, a.image);
         }
     }
+    homeButtonStatus = 0;
+    homeButtonFunc();
 }
 makeHomeReal();
 
@@ -99,7 +112,6 @@ for (let a of array) {
 
 }
 
-console.log(array);
 
 function getChildNames(topName) {
     let topId;
@@ -116,6 +128,14 @@ function getChildNames(topName) {
         }
     }
     return childNames;
+}
+
+function getImageName(name) {
+    for (let a of array) {
+        if (a.name.toLowerCase() == name.toLowerCase()) {
+            return a.image;
+        }
+    }
 }
 
 function getChilds(topName) {
@@ -136,18 +156,17 @@ function getChilds(topName) {
 }
 
 let personCards = document.querySelectorAll('.personCard');
-let topElement = document.querySelector('.top_element');
 
+topElement = document.querySelector('.top_element');
 
-
-
-
-for (var i = 0; i < personCards.length; i++) {
-  personCards[i].onclick = function(){
+function handler1(str){
+    
+   
+    
     homeButtonStatus = 1;
-    homeButtonFunc();  
-    let name = getNamefromHtml(this.querySelector('.avatar').src);
-    topElement.src = changePicName(topElement.src, name);
+    homeButtonFunc();
+    let name = str;
+    topElement.src = changePicName(topElement.src, getImageName(name));
     document.querySelector('.top_element_outer').classList.add('gradient-border');
     document.querySelector('.top_element_outer').classList.add('top_element_round_size');
     document.querySelector('.top_element').classList.add('top_element_round_size');
@@ -161,12 +180,18 @@ for (var i = 0; i < personCards.length; i++) {
     
     personCards = document.querySelectorAll('.person-card');
         
-        for (let a of personCards)
-        a.onclick = function(){
-            alert('Click!!');
-        }
-  }
-
+        // for (let a of personCards)
+        // a.onclick = function(){
+            // alert('Click!!');
+        // }
 }
+
+
+
+
+  
+
+
+
 
 
